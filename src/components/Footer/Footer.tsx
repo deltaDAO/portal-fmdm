8773c54025bf2540a9e19ea0fc73bfaf3c2f06e9
KevinRohlf <123676073+KevinRohlf@users.noreply.github.com>
2024-11-13 09:36:55 +0100

    chore: update footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,37 +1,90 @@
+import Button from '@components/@shared/atoms/Button'
+import Container from '@components/@shared/atoms/Container'
+import Logo from '@components/@shared/atoms/Logo'
+import Markdown from '@components/@shared/Markdown'
+import { useMarketMetadata } from '@context/MarketMetadata'
+import { useUserPreferences } from '@context/UserPreferences'
+import { useGdprMetadata } from '@hooks/useGdprMetadata'
 import { ReactElement } from 'react'
 import styles from './Footer.module.css'
-import Markdown from '@shared/Markdown'
-import Links from './Links'
-import { useMarketMetadata } from '@context/MarketMetadata'
-import DeltaDaoLogo from '@images/deltaDAO_Logo_small_RGB_white.svg'
-import Container from '@components/@shared/atoms/Container'
+import LogoAssetInverted from '@images/fmdm-logo-white.svg'
 
 export default function Footer(): ReactElement {
-  const { siteContent } = useMarketMetadata()
-  const { siteTitle, footer } = siteContent
-  const { copyright, subtitle } = footer
+  const { appConfig, siteContent } = useMarketMetadata()
+  const { copyright, footer } = siteContent
+  const { setShowPPC } = useUserPreferences()
+
+  const cookies = useGdprMetadata()
+
+  const year = new Date().getFullYear()
 
   return (
     <footer className={styles.footer}>
       <Container className={styles.container}>
-        <div>
-          <p className={styles.siteTitle}>{siteTitle}</p>
-          <a
-            href="https://delta-dao.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div className={styles.main}>
-              <DeltaDaoLogo />
-              <p className={styles.subtitle}>{subtitle}</p>
-            </div>
-          </a>
+        <div className={styles.logo}>
+          <LogoAssetInverted />
         </div>
-        <Links />
+        <div className={styles.content}>
+          <div className={styles.links}>
+            {footer.links.map((e) => (
+              <Button
+                key={e.label}
+                style="text"
+                className={styles.link}
+                to={e.link}
+              >
+                {e.label}
+              </Button>
+            ))}
+          </div>
+          <div className={styles.copyrightContainer}>
+            <div className={styles.copyright}>
+              <span>
+                <Markdown text={footer.designedBy} />
+              </span>
+              <span className={styles.legal}>
+                <Markdown text={`&copy; ${year} ${copyright}`} />
+              </span>
+            </div>
+            <div className={styles.legal}>
+              <Button
+                className={styles.link}
+                style="text"
+                size="small"
+                href="https://portal.pontus-x.eu/imprint"
+              >
+                Imprint
+              </Button>
+              {' — '}
+              <Button
+                className={styles.link}
+                style="text"
+                size="small"
+                href="https://portal.pontus-x.eu/privacy/en"
+              >
+                Privacy
+              </Button>
+              {appConfig.privacyPreferenceCenter === 'true' && (
+                <>
+                  {' — '}
+                  <Button
+                    style="text"
+                    size="small"
+                    className="link"
+                    onClick={() => {
+                      setShowPPC(true)
+                    }}
+                  >
+                    {cookies.optionalCookies?.length > 0
+                      ? 'Cookie Settings'
+                      : 'Cookies'}
+                  </Button>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
       </Container>
-      <div className={styles.copyright}>
-        <Markdown text={copyright} />
-      </div>
     </footer>
   )
 }

867a9f9aba36b81d424b8a54ec2c8dc2453d8216
KevinRohlf <123676073+KevinRohlf@users.noreply.github.com>
2024-11-19 14:44:01 +0100

    chore: remove gaia-x filter

--- src/@types/ComplianceType.ts
+++ src/@types/ComplianceType.ts
@@ -1,14 +1,10 @@
 export enum ComplianceType {
-  Gaia_X = 'gaia-x',
   FMDM = 'fmdm'
 }
 
 export class ComplianceTypeLookup {
   public static getCaption(complianceType: ComplianceType): string {
     const lookupTable: Record<ComplianceType, string> = {
-      [ComplianceType.Gaia_X]:
-        process.env.NEXT_PUBLIC_COMPLIANCE_TYPE_GAIA_X_CAPTION ||
-        'Gaia-X compliant',
       [ComplianceType.FMDM]:
         process.env.NEXT_PUBLIC_COMPLIANCE_TYPE_FMDM_CAPTION || 'FMDM compliant'
     }
